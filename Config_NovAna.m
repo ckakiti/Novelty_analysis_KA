14cf84f2d4755ee6bcaf917a95955eb4b6fc9348
ckakiti <akiti@g.harvard.edu>
2019-02-15 14:21:30 -0500

    new file

--- Config_NovAna.m
+++ Config_NovAna.m
@@ -1,14 +1,14 @@
-networkname_format='DeepCut_resnet50_noveltyMay21shuffle1_700000'; % rubber toy orig
+% networkname_format='DeepCut_resnet50_noveltyMay21shuffle1_700000'; % rubber toy orig
 % networkname_format='DeepCut_resnet50_MoSeqNoveltySep12shuffle1_1030000';
 % networkname_format='DeepCut_resnet50_MoSeqNovelty_RetrainSep17shuffle1_1030000';
-% networkname_format='DeepCut_resnet50_MoSeqConfiguralDec04shuffle1_1030000';
+networkname_format='DeepCut_resnet50_MoSeqConfiguralDec04shuffle1_1030000';
 % networkname_format='DeepCut_resnet50_MoSeqWithOldArenaDec31shuffle1_1030000';
 videoname_format='XYZ_YYMMDD_rgb.mp4'; %avi';
 
 video_xlen=424; % 420;
 video_ywid=512; % 520;                     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-fpm = 1800;             % frames per minute
+fpm = 1500;             % frames per minute
 fps = fpm./60;          % frames per second
 ppc = 42/6.3; %123/23; %340/(2*30);   % pixels per cm   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 radius_cm = 8; %10 for tea obj  % Time spent around the obj radius (cm)   %%%%%%%%%%%%%
