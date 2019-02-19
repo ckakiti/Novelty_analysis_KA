0637c9dc88d9ad8f92c6852ee587cfa2bde5c1dd
ckakiti <akiti@g.harvard.edu>
2019-02-19 13:50:28 -0500

    -

--- FurtherAnalysis/Gather_trajectories.m
+++ FurtherAnalysis/Gather_trajectories.m
@@ -3,22 +3,22 @@
 clear
 clc
 
-currMouse = 'Miami';
+currMouse = 'S6_Wedge';
 
-cd(['/home/alex/Programs/DeepLabCut_new/DeepLabCut/videos/Miami_DLC/' ...
+cd(['/home/alex/Programs/DeepLabCut_new/DeepLabCut/videos/NewHope-ROTJ/' ...
     currMouse])% '/181223/'])
 cd Analyzed_Data;
 
 trajs = dir('Trajectory*.tif'); % dir('Heatmap*.tif');
 %sessionNames = {'1' '2' '3' '4' '5' '6' '7' '8' 'N1' 'N2' 'N3' 'N4'};
-sessionNames = {'H1' 'H2' 'N1' 'N2' 'N3' 'N4' 'N5'};% 'N6' 'N7' 'N8'};
+sessionNames = {'H1' 'H2' 'N1' 'N2' 'N3' 'N4' 'N5' 'N6' 'N7' 'N8' 'N9' 'N10'};
 
 fig1 = figure(1);
-suptitle(currMouse)
+suptitle([currMouse(1:2), ' ' currMouse(4:end)]) % currMouse)
 set(fig1, 'Position', [200 1 1500 800]);
 hold on
 
-for i = 1:length(trajs)
+for i = 1:8 %length(trajs)
     image = imread(trajs(i).name);
     imageCropped = imcrop(image, [550 150 1150 950]);
     
@@ -34,5 +34,5 @@ for i = 1:length(trajs)
 end
 
 %saveas(fig1, ['Trajectories: ' currMouse])
-%saveas(fig1, ['Trajectories: ' currMouse '.tif'])
+%saveas(fig1, ['Trajectories: ' currMouse '_body.tif'])
 %disp('Saved')
