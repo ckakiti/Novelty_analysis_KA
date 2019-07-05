6033ffa64f04636491fba33866d0443ad41ae2cc
ckakiti <akiti@g.harvard.edu>
2019-07-05 16:49:27 -0400

    -

--- Config_NovAna.m
+++ Config_NovAna.m
@@ -4,25 +4,27 @@
 % networkname_format='DeepCut_resnet50_MoSeqConfiguralDec04shuffle1_1030000';
 % networkname_format='DeepCut_resnet50_MoSeqWithOldArenaDec31shuffle1_1030000';
 % networkname_format='DeepCut_resnet50_MoSeqConfiguralUpdateMar04shuffle1_1030000';
-networkname_format='DeepCut_resnet50_MoSeqFPApr24shuffle1_1030000';
+% networkname_format='DeepCut_resnet50_MoSeqFPApr24shuffle1_1030000';
+% networkname_format='DeepCut_resnet50_MitsukoOperantLearningSep15shuffle1_1030000';
+networkname_format='DeepCut_resnet50_MitsukoOperant_FPJun18shuffle1_1030000';
 
-videoname_format='XYZ_YYMMDD_rgb.avi'; %mp4; %avi';
+videoname_format='XYZ_YYMMDD_rgb.mp4'; %mp4; %avi';
 
-video_xlen=512;%520;%512;
-video_ywid=424;%420;%424;            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+video_xlen=400;%520;%512;%400
+video_ywid=480;%420;%424;%480            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-fpm = 900; %1800 %1500 %900 % frames per minute
+fpm = 5040; %5040 %1800 %1500 %900 % frames per minute
 fps = fpm./60;          % frames per second
 ppc = 42/6.3; %123/23; %340/(2*30);   % pixels per cm   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-radius_cm = 9; %10 for tea obj %9 for MoSeq %8 default  % Time spent around the obj radius (cm)   %%%%%%%%%%%%%
+radius_cm = 5; %10 for tea obj %9 for MoSeq %8 default %5-8 nose  % Time spent around the obj radius (cm)   %%%%%%%%%%%%%
 radius = radius_cm.*ppc;% Time spent around the obj radius (pixels)
 angle_radius = 15;      % Time orient towards the obj radius (degree)
 
 % Calculation Parameters
 Dis_ts_frame=500;          % Time spent around the obf start and end frame
-Dis_te_frame=10.*60.*fps+Dis_ts_frame;
+Dis_te_frame=8.*60.*fps+Dis_ts_frame; %10 %8
 
 Ang_ts_frame=500;         % Time orient towards the obj start and end frame
-Ang_te_frame=10.*60.*fps+Ang_ts_frame;
+Ang_te_frame=8.*60.*fps+Ang_ts_frame; %10 %8
 
 
