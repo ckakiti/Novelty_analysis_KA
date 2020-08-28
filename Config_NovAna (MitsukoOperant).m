35d4aae1cbbcf28636798596dd9e010783700843
ckakiti <akiti@g.harvard.edu>
2020-08-28 10:09:17 -0400

    -

---
+++ Config_NovAna (MitsukoOperant).m
@@ -0,0 +1,30 @@
+% networkname_format='DeepCut_resnet50_noveltyMay21shuffle1_700000'; % rubber toy orig
+% networkname_format='DeepCut_resnet50_MoSeqNoveltySep12shuffle1_1030000';
+% networkname_format='DeepCut_resnet50_MoSeqNovelty_RetrainSep17shuffle1_1030000';
+% networkname_format='DeepCut_resnet50_MoSeqConfiguralDec04shuffle1_1030000';
+% networkname_format='DeepCut_resnet50_MoSeqWithOldArenaDec31shuffle1_1030000';
+% networkname_format='DeepCut_resnet50_MoSeqConfiguralUpdateMar04shuffle1_1030000';
+% networkname_format='DeepCut_resnet50_MoSeqFPApr24shuffle1_1030000';
+networkname_format='DeepCut_resnet50_MitsukoOperantLearningSep15shuffle1_1030000';
+% networkname_format='DeepCut_resnet50_MitsukoOperant_FPJun18shuffle1_1030000';
+% networkname_format = 'DeepCut_resnet50_MoSeqGreyMouseJul23shuffle1_1030000';
+% networkname_format = 'DeepCut_resnet50_Sandra_PupilsOct17shuffle1_1030000';
+
+videoname_format='XYZ_YYMMDD_rgb.mp4'; %mp4; %avi';
+
+video_xlen=640;%520;%512;%400%320
+video_ywid=480;%420;%424;%480%240            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fpm = 900; %12900 %5040 %1800 %1500 %900 % frames per minute
+fps = fpm./60;          % frames per second
+ppc = 42/6.3; %123/23; %340/(2*30);   % pixels per cm   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+radius_cm = 8; %10 for tea obj %9 for MoSeq %8 default %6 Mit_FP  % Time spent around the obj radius (cm)   %%%%%%%%%%%%%
+radius = radius_cm.*ppc;% Time spent around the obj radius (pixels)
+angle_radius = 15;      % Time orient towards the obj radius (degree)
+
+% Calculation Parameters
+Dis_ts_frame=500; % Time spent around the obj start and end frame
+Dis_te_frame=10.*60.*fps+Dis_ts_frame;
+
+Ang_ts_frame=500; % Time orient towards the obj start and end frame
+Ang_te_frame=10.*60.*fps+Ang_ts_frame;
