9069a7c42a6685bd89269610aa889702144e5876
ckakiti <25230644+ckakiti@users.noreply.github.com>
2020-09-04 12:12:17 -0400

    Add files via upload

---
+++ MoSeqAnalysis/uuid_list.m
@@ -0,0 +1,25 @@
+%% create extract .m file with each uuid, 
+%       name, and novelty condition (.mat -> .m)
+
+clear
+clc
+close all
+
+setName = 'Dataset_20190723';
+whichDay = 3; % 3=N1
+
+Mice_Index_path = ['/Users/cakiti/Dropbox (Uchida Lab)/Korleki Akiti/Behavior/Standard setup/' ...
+   'CombineAnalysis/Just-in-case files/Dataset_20190723/MiceIndex.mat'];
+load(Mice_Index_path)
+
+Mice_uuid_N1 = struct('name', {Mice.name}, ...
+                      'novelty', {Mice.novelty});
+for mouseiter = 1:length(Mice)
+    Mice_uuid_N1(mouseiter).uuid = Mice(mouseiter).ExpDay(whichDay).MSid;
+end
+
+Mice_uuid_N1_table = cell2table({Mice_uuid_N1.uuid}');
+if(0)
+    writetable(Mice_uuid_N1_table, 'MiceIndex_combine3_uuidN1.csv', ...
+        'WriteVariableNames', 0)
+end
